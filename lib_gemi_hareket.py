9dbcd5fd750c2aeb31e9a55642627814a241b4f3
yilmazm <yilmazmu15@itu.edu.tr>
2018-09-17 21:43:48 +0300

    Gemi harekete YAPILACAKLAR eklendi.

--- lib_gemi_hareket.py
+++ lib_gemi_hareket.py
@@ -1,33 +1,42 @@
+# TODO Sağ/Sol motor pinleri de fonksiyonlarda alınacak. Değişken silinecek.
+
 import lib_dc_motor as dc
 
-solMotorPins = [8,10,12]
-sagMotorPins = [11,13,15]
+solMotorPins = [8, 10, 12]
+sagMotorPins = [11, 13, 15]
 
-def motorlari_ayarla():
-	dc.pin_ayarla(solMotorPins)
-	dc.pin_ayarla(sagMotorPins)
-
-def sola_don():
-	dc.ileri(sagMotorPins)
-	dc.geri(solMotorPins)
-
-def saga_don():
-	dc.ileri(solMotorPins)
-	dc.geri(sagMotorPins)
-
-def ileri():
-	dc.ileri(solMotorPins)
-	dc.ileri(sagMotorPins)
-
-def dur():
-	dc.durdur(solMotorPins)
-	dc.durdur(sagMotorPins)
-
-def geri():
-	dc.geri(solMotorPins)
-	dc.geri(sagMotorPins)
 
 def motor_pinleri_ayarla(solPins, sagPins):
-	solMotorPins = solPins
-	sagMotorPins = sagPins
-	motorlari_ayarla()
+    solMotorPins = solPins
+    sagMotorPins = sagPins
+    motorlari_ayarla()
+
+
+def motorlari_ayarla():
+    dc.pin_ayarla(solMotorPins)
+    dc.pin_ayarla(sagMotorPins)
+
+
+def sola_don():
+    dc.ileri(sagMotorPins)
+    dc.geri(solMotorPins)
+
+
+def saga_don():
+    dc.ileri(solMotorPins)
+    dc.geri(sagMotorPins)
+
+
+def ileri():
+    dc.ileri(solMotorPins)
+    dc.ileri(sagMotorPins)
+
+
+def dur():
+    dc.durdur(solMotorPins)
+    dc.durdur(sagMotorPins)
+
+
+def geri():
+    dc.geri(solMotorPins)
+    dc.geri(sagMotorPins)
