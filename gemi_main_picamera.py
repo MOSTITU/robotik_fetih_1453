05a266a13c39bcd5fdbbe6085bee93bd8066c293
yilmazm <yilmazmu15@itu.edu.tr>
2018-09-18 16:39:21 +0300

    Kapı tespit etme deneme 1

--- gemi_main_picamera.py
+++ gemi_main_picamera.py
@@ -8,10 +8,8 @@ import os
 import RPi.GPIO as GPIO
 import lib_tespit_sonrasi as ts
 
-
 GPIO.setmode(GPIO.BOARD)
 gh.motorlari_ayarla()
-
 
 # Kamera kutuphanesinden bir nesne al ve kamera degiskenine ata
 kamera = PiCamera()
@@ -35,22 +33,32 @@ time.sleep(0.1)
 # Bir for dongusu icinde kameradan resim yakalamaya basla
 for resimKaresi in kamera.capture_continuous(resimBellegi, format="bgr", use_video_port=True):
     # Resim karesinin piksellerini resim isimli bir degiskene yaz
-    resim = resimKaresi.array
+    anaResim = resimKaresi.array
+    anaResim = cv2.resize(anaResim, (340, 220))
 
-    resim = cv2.resize(resim, (340, 220))
+    # gemiResim = anaResim.copy()
+    # gemiResim = cv2.resize(gemiResim, (340, 220))
+    # gemiMaske = yar.maske_olustur(gemiResim, yar.renk_siniri["yesil"], yar.cekirdek)
+    # gemiAlanlar = yar.cerceve_ciz(gemiResim, gemiMaske)
+    # enBuyukGemi = yar.en_buyugu_bul(gemiAlanlar)
+    # # cismin etrafına dikdörtgen çizme
+    # cv2.rectangle(gemiResim, (enBuyukGemi['solUstKose'][0], enBuyukGemi['solUstKose'][1]),
+    #               (enBuyukGemi["sagAltKose"][0], enBuyukGemi["sagAltKose"][1]), (255, 0, 0), 3)
+    # ts.goruntuye_gore_hareket(gemiResim, enBuyukGemi)
 
-    gemiMaske = yar.maske_olustur(resim, yar.renk_siniri["yesil"], yar.cekirdek)
-    gemiAlanlar = yar.cerceve_ciz(resim, gemiMaske)
+    kapiResim = anaResim.copy()
+    kapiResim = cv2.resize(kapiResim, (340, 220))
+    kapiMaske = yar.maske_olustur(kapiResim, yar.renk_siniri["yesil"], yar.cekirdek)
+    kapiAlanlar = yar.cerceve_ciz(kapiResim, kapiMaske)
+    _, kapiMerkez, kapiYon, s1, s2 = yar.kapiyi_tespit_et(kapiAlanlar)
+    # Sütunları dikdörtgen içine alma
+    cv2.rectangle(kapiResim, (s1['solUstKose'][0], s1['solUstKose'][1]),
+                  (s1["sagAltKose"][0], s1["sagAltKose"][1]), (255, 0, 0), 3)
+    cv2.rectangle(kapiResim, (s2['solUstKose'][0], s2['solUstKose'][1]),
+                  (s2["sagAltKose"][0], s2["sagAltKose"][1]), (255, 0, 0), 3)
 
-    # print("Tespit edilen cisim sayisi:", len(gemiAlanlar))
-    enBuyukGemi = yar.en_buyugu_bul(gemiAlanlar)
-
-    # cismin etrafına dikdörtgen çizme
-    cv2.rectangle(resim, (enBuyukGemi['solUstKose'][0], enBuyukGemi['solUstKose'][1]),
-                  (enBuyukGemi["sagAltKose"][0], enBuyukGemi["sagAltKose"][1]), (255, 0, 0), 3)
-    ts.goruntuye_gore_hareket(resim, enBuyukGemi)
-
-    cv2.imshow("Video", resim)
+    # cv2.imshow("Gemiler", gemiResim)
+    cv2.imshow("Kapı", kapiResim)
     resimBellegi.truncate(0)
     if cv2.waitKey(20) == 27:
         break
