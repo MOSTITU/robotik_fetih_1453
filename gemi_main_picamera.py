30c1bfadaeef29ad7a0efd3aa58d5052ea517cbb
yilmazm <yilmazmu15@itu.edu.tr>
2018-09-19 04:31:27 +0300

    Cihazlar ekleniyor... Bantı döndürmesi için step motor pinleri ve step motor çalıştırması eklendi. Çarpraz ve dikey mesafe sensörlerinin pinleri ayarlandı

--- gemi_main_picamera.py
+++ gemi_main_picamera.py
@@ -2,20 +2,30 @@ from picamera import PiCamera
 from picamera.array import PiRGBArray
 import time
 import cv2
-import lib_gemi_hareket as gh
 import RPi.GPIO as GPIO
+import lib_step_motor as sm
+import lib_gemi_hareket as gh
+import lib_mesafe_sensoru as ms
 import lib_tespit_ve_hareket as th
+
+DIK_SU_MESAFESI = 26
 
 GPIO.setmode(GPIO.BOARD)
 gh.motorlari_ayarla()
+# Çarpraz sensör
+ms.pin_ayarla(38, 40)
+# Direk aşağı sensör
+ms.pin_ayarla(32, 36)
+STEP_PIN = [31, 33, 35, 37]
+sm.motor_pinlerini_ayarla(STEP_PIN)
 
 # Kamera kutuphanesinden bir nesne al ve kamera degiskenine ata
 kamera = PiCamera()
 
 # Kayıt/İşlem çözünürlüğü
-kayitCozunurlugu = [640, 480]
-islemCozunurlugu = [340, 220]
-kamera.resolution = (kayitCozunurlugu[0], kayitCozunurlugu[1])
+kameraCozunurlugu = [640, 480]
+islemeCozunurlugu = [340, 220]
+kamera.resolution = (kameraCozunurlugu[0], kameraCozunurlugu[1])
 
 # Kameranin saniyedeki resim sayisini 50 olarak ayarla
 kamera.framerate = 50
@@ -24,7 +34,7 @@ kamera.framerate = 50
 # kamera.vflip = True
 
 # Resimleri tutmak icin bellekte yer ac
-resimBellegi = PiRGBArray(kamera, size=(kayitCozunurlugu[0], kayitCozunurlugu[1]))
+resimBellegi = PiRGBArray(kamera, size=(kameraCozunurlugu[0], kameraCozunurlugu[1]))
 
 # Kameranin hazir olmasi icin biraz bekle
 time.sleep(0.1)
@@ -32,11 +42,11 @@ time.sleep(0.1)
 # Video kaydı için
 kayitTime = time.strftime("%Y-%m-%d-%H-%M")
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-temizKayit = cv2.VideoWriter('./Medya/Kayitlar/' + kayitTime + 'temizKayit.avi', fourcc, 10.0, (kayitCozunurlugu[0], kayitCozunurlugu[1]))
-duvarKayit = cv2.VideoWriter('./Medya/Kayitlar/' + kayitTime + 'duvarKayit.avi', fourcc, 10.0, (islemCozunurlugu[0], islemCozunurlugu[1]))
-gemiKayit = cv2.VideoWriter('./Medya/Kayitlar/' + kayitTime + 'gemiKayit.avi', fourcc, 10.0, (islemCozunurlugu[0], islemCozunurlugu[1]))
-kapiKayit = cv2.VideoWriter('./Medya/Kayitlar/' + kayitTime + 'kapiKayit.avi', fourcc, 10.0, (islemCozunurlugu[0], islemCozunurlugu[1]))
-surKayit = cv2.VideoWriter('./Medya/Kayitlar/' + kayitTime + 'surKayit.avi', fourcc, 10.0, (islemCozunurlugu[0], islemCozunurlugu[1]))
+temizKayit = cv2.VideoWriter('./Medya/Kayitlar/' + kayitTime + 'temizKayit.avi', fourcc, 10.0, (kameraCozunurlugu[0], kameraCozunurlugu[1]))
+duvarKayit = cv2.VideoWriter('./Medya/Kayitlar/' + kayitTime + 'duvarKayit.avi', fourcc, 10.0, (islemeCozunurlugu[0], islemeCozunurlugu[1]))
+gemiKayit = cv2.VideoWriter('./Medya/Kayitlar/' + kayitTime + 'gemiKayit.avi', fourcc, 10.0, (islemeCozunurlugu[0], islemeCozunurlugu[1]))
+kapiKayit = cv2.VideoWriter('./Medya/Kayitlar/' + kayitTime + 'kapiKayit.avi', fourcc, 10.0, (islemeCozunurlugu[0], islemeCozunurlugu[1]))
+surKayit = cv2.VideoWriter('./Medya/Kayitlar/' + kayitTime + 'surKayit.avi', fourcc, 10.0, (islemeCozunurlugu[0], islemeCozunurlugu[1]))
 
 # Gemi toplama/boşaltma işlemi için
 gemiTopla = True
@@ -47,7 +57,7 @@ for resimKaresi in kamera.capture_continuous(resimBellegi, format="bgr", use_vid
     # Resim karesinin piksellerini resim isimli bir degiskene yaz
     anaResim = resimKaresi.array
     temizKayit.write(anaResim)
-    anaResim = cv2.resize(anaResim, (islemCozunurlugu[0], islemCozunurlugu[1]))
+    anaResim = cv2.resize(anaResim, (islemeCozunurlugu[0], islemeCozunurlugu[1]))
 
     if gemiTopla:
         gemiResim = th.gemi_bul_ve_hareket_et(anaResim)
@@ -59,6 +69,7 @@ for resimKaresi in kamera.capture_continuous(resimBellegi, format="bgr", use_vid
         kapiKayit.write(kapiResim)
         cv2.imshow("Kapı", kapiResim)
 
+    sm.tam_tur_don(0.01, 1, STEP_PIN)
     duvarResim = th.duvar_bul_ve_carpma(anaResim)
 
     cv2.imshow("Temiz Görüntü", anaResim)
